1dfd4766022261e19ff8b6f91cd028a9efc762fd
Ciara Molloy <ciaraevemolloy94@gmail.com>
2026-06-28 13:56:17 +0100

    Movie Details page components

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -19,3 +19,10 @@ export interface BaseMovieProps {
 export interface BaseMovieListProps { 
     movies: BaseMovieProps[];
   }   
+
+export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+  }
